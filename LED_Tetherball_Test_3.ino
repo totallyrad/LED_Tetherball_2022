e543050ade030fd928973be7c6536eee22188bdd
Ian K <ian.kawabata@gmail.com>
2022-05-13 15:06:15 -0700

    Update LED_Tetherball_Test_3.ino

--- LED_Tetherball_Test_3.ino
+++ LED_Tetherball_Test_3.ino
@@ -1,18 +1,5 @@
 #include <FastLED.h>
 
-/*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
-*/
-
-
 #define DATA_PIN    3
 //#define CLK_PIN   4
 #define LED_TYPE    WS2811
